7b0a7ecdb42f66db6ef9b0e4c72af88a2f1903f5
Spencer Rudnick <therudnick@gmail.com>
2018-09-25 22:50:44 -0500

    json testing

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,13 @@ import * as ReactDOM from 'react-dom';
 import { SynthKitchen } from './ts/SynthKitchen';
 
 import './index.css';
+import * as People from './test.json';
 
 export const context = new AudioContext();
+
+People.forEach((person: any) => {
+  console.log(`${person}`);
+});
 
 ReactDOM.render(
   <SynthKitchen />,
