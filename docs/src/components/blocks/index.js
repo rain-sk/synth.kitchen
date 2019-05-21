1784849c7a91328967c64b01a34814421999ee26
Spencer Rudnick <spencerudnick@gmail.com>
2019-05-21 08:00:27 +0200

    remove docs and add /build to gitignroe

--- docs/src/components/blocks/index.js
+++
@@ -1,5 +0,0 @@
-export { default as ModBlock } from './ModBlock';
-export { default as OscBlock } from './OscBlock';
-export { default as LfoBlock } from './LfoBlock';
-export { default as GainBlock } from './GainBlock';
-export { default as OutputBlock } from './OutputBlock';
