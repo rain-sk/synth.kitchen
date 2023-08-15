c9c9e3b4faadbb3b0e44b55503c8be41bbe214db
Spence <spencer@synth.kitchen>
2023-08-15 21:52:51 +0200

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	build: {
-		rollupOptions: {
-			output: {
-				compact: true
-			}
-		}
-	},
-	plugins: [react()]
-});
+  plugins: [react()],
+})
