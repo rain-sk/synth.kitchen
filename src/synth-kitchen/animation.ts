79bac2336020b034181ae21060834a3c8fb23ccc
Spence <spencer@synth.kitchen>
2023-08-21 19:56:56 +0200

    prettier-format animation.ts

--- src/synth-kitchen/animation.ts
+++ src/synth-kitchen/animation.ts
@@ -1,20 +1,16 @@
-
 type AnimationCallback = () => void;
 
 const callbackQueue: AnimationCallback[] = [];
 
-
 const flushCallbackQueue = () => {
-    for (const callback of callbackQueue.splice(0, callbackQueue.length)) {
-        callback();
-    }
-}
+	for (const callback of callbackQueue.splice(0, callbackQueue.length)) {
+		callback();
+	}
+};
 
-
-export const queueAnimation =
-    (callback: AnimationCallback) => {
-        if (callbackQueue.length === 0) {
-            requestAnimationFrame(flushCallbackQueue);
-        }
-        callbackQueue.push(callback);
-    }
+export const queueAnimation = (callback: AnimationCallback) => {
+	if (callbackQueue.length === 0) {
+		requestAnimationFrame(flushCallbackQueue);
+	}
+	callbackQueue.push(callback);
+};
