aafb8cbb747615edc2afb1af3d93ee3166820049
Spence <spencer@synth.kitchen>
2023-08-22 10:28:14 +0200

    give gain a param and connectors

--- src/synth-kitchen/modules/gain.tsx
+++ src/synth-kitchen/modules/gain.tsx
@@ -1,10 +1,12 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import { IAudioContext, IGainNode } from 'standardized-audio-context';
 import { audioContext } from '../audio';
 import { useModuleState } from '../hooks/use-module-state';
 
 import { IModule, IModuleState } from '../state/types/module';
+import { IoConnectors } from '../components/io-connectors';
+import { NumberParameter } from '../components/number-parameter';
 
 const gainStateFromNode = (
 	node: IGainNode<IAudioContext>
@@ -29,12 +31,53 @@ export const GainModule: React.FC<{ module: IModule<'GAIN'> }> = ({
 	module
 }) => {
 	const gainRef = useRef<IGainNode<IAudioContext>>();
-	const [state] = useModuleState<'GAIN'>(
+	const [state, setState] = useModuleState<'GAIN'>(
 		() => initGain(gainRef, module.state),
 		module.moduleKey
 	);
 
 	const enabled = state != undefined;
 
-	return enabled ? <p>enabled</p> : <p>loading...</p>;
+	const inputAccessor = useCallback(() => gainRef.current as any, [enabled]);
+
+	const outputAccessor = useCallback(() => gainRef.current as any, [enabled]);
+
+	const commitGainChange = useCallback(
+		(gain: number) => {
+			gainRef.current?.gain.linearRampToValueAtTime(
+				gain,
+				audioContext.currentTime
+			);
+			setState({
+				...state,
+				gain
+			});
+		},
+		[audioContext, state]
+	);
+
+	const gainAccessor = useCallback(
+		() => gainRef.current?.gain as any,
+		[enabled]
+	);
+
+	return enabled ? (
+		<>
+			<IoConnectors
+				moduleKey={module.moduleKey}
+				inputAccessors={[inputAccessor]}
+				outputAccessors={[outputAccessor]}
+			/>
+
+			<section>
+				<NumberParameter
+					moduleKey={module.moduleKey}
+					paramAccessor={gainAccessor}
+					name="gain"
+					value={state.gain}
+					commitValueCallback={commitGainChange}
+				/>
+			</section>
+		</>
+	) : null;
 };
