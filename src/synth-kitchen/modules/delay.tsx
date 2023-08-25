698e087719a25eca97daa453650eeb50a3729faf
Spence <spencer@synth.kitchen>
2023-08-25 12:01:40 +0200

    delay module

--- src/synth-kitchen/modules/delay.tsx
+++ src/synth-kitchen/modules/delay.tsx
@@ -1,10 +1,12 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import { IAudioContext, IDelayNode } from 'standardized-audio-context';
 import { audioContext } from '../audio';
 import { useModuleState } from '../hooks/use-module-state';
 
 import { IModule, IModuleState } from '../state/types/module';
+import { IoConnectors } from '../components/io-connectors';
+import { NumberParameter } from '../components/number-parameter';
 
 const delayStateFromNode = (
 	node: IDelayNode<IAudioContext>
@@ -32,12 +34,55 @@ export const DelayModule: React.FC<{ module: IModule<'DELAY'> }> = ({
 	module
 }) => {
 	const delayRef = useRef<IDelayNode<IAudioContext>>();
-	const [state] = useModuleState<'DELAY'>(
+	const [state, setState] = useModuleState<'DELAY'>(
 		() => initDelay(delayRef, module.state),
 		module.moduleKey
 	);
 
-	const enabled = state != undefined;
+	const enabled = !!state;
+
+	const inputAccessor = useCallback(() => delayRef.current as any, [enabled]);
+
+	const outputAccessor = useCallback(() => delayRef.current as any, [enabled]);
+
+	const commitTimeChange = useCallback(
+		(delayTime: number) => {
+			delayRef.current?.delayTime.linearRampToValueAtTime(
+				delayTime,
+				audioContext.currentTime
+			);
+			setState({
+				...state,
+				delayTime
+			});
+		},
+		[audioContext, state]
+	);
+
+	const delayTimeAccessor = useCallback(
+		() => delayRef.current?.delayTime as any,
+		[enabled]
+	);
+
+	return enabled ? (
+		<>
+			<IoConnectors
+				moduleKey={module.moduleKey}
+				inputAccessors={[inputAccessor]}
+				outputAccessors={[outputAccessor]}
+			/>
+
+			<section>
+				<NumberParameter
+					moduleKey={module.moduleKey}
+					paramAccessor={delayTimeAccessor}
+					name="time"
+					value={state.delayTime}
+					commitValueCallback={commitTimeChange}
+				/>
+			</section>
+		</>
+	) : null;
 
 	return enabled ? <p>enabled</p> : <p>loading...</p>;
 };
