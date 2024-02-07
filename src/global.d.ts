e861918cfa2a59b05b588d95fb956b6fb6b48083
Rain <rain@synth.kitchen>
2024-02-07 13:06:12 +0100

    block the word "slave" for project names

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,2 +1,1 @@
-declare module 'project-name-generator';
 declare module 'uuid';
--- src/synth-kitchen/utils/random-name.ts
+++ src/synth-kitchen/utils/random-name.ts
@@ -1,8 +1,9 @@
-import generate from 'project-name-generator';
+import generate from 'server-name-gen';
 
 export const randomName = (customNoun?: string): string => {
 	if (!customNoun) {
-		return generate({ words: 3, alliterative: true }).dashed;
+		return generate({ words: 3, alliterative: true, blocklist: ['slave'] })
+			.dashed;
 	} else {
 		const randomName = generate({ words: 3 }).raw;
 		randomName[2] = customNoun;
