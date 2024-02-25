7d060136f06301076c6b5a56bde2b39009e70fb4
Rain <rain@synth.kitchen>
2024-02-25 10:48:51 +0100

    move state into `useRef`s

--- src/components/module-canvas-backdrop.tsx
+++ src/components/module-canvas-backdrop.tsx
@@ -21,22 +21,22 @@ const positionFromMouseEvent = (
 	e.clientY + scrollableElement.scrollTop
 ];
 
-const state = {
-	container: undefined as HTMLElement | undefined,
-	selection: {
-		element: undefined as HTMLDivElement | undefined,
-		start: INVALID_POSITION,
-		end: INVALID_POSITION
-	},
-	spacer: undefined as HTMLDivElement | undefined
-};
-
 export const ModuleCanvasBackdrop: React.FC<{
 	drawOnTop: boolean;
 	children: React.ReactNode;
 }> = ({ children }) => {
-	const { modules, selectedModuleKeys } = useStateContext();
+	const { modules, modulePositions, selectedModuleKeys } = useStateContext();
 	const dispatch = useDispatchContext();
+
+	const container = useRef(undefined as HTMLElement | undefined);
+	const spacer = useRef(undefined as HTMLDivElement | undefined);
+	const selection = useRef({
+		element: undefined as HTMLDivElement | undefined,
+		start: INVALID_POSITION,
+		end: INVALID_POSITION
+	});
+	const isInitialized = () =>
+		!!container.current && !!spacer.current && !!selection.current.element;
 
 	const [initialized, setInitialized] = useState(false);
 
@@ -45,57 +45,55 @@ export const ModuleCanvasBackdrop: React.FC<{
 
 	useEffect(() => {
 		setDeviceButtonPosition(INVALID_POSITION);
-	}, [modules]);
+	}, [modules, modulePositions]);
+
 	useEffect(() => {
 		if (selectedModuleKeys.size !== 0) {
 			setDeviceButtonPosition(INVALID_POSITION);
 		}
-	}, [deviceButtonPosition, selectedModuleKeys.size]);
+	}, [selectedModuleKeys.size]);
 
 	const drawSelection = useCallback(() => {
-		if (state.selection.element) {
-			if (state.selection.start.join(',') === INVALID_POSITION.join(',')) {
-				state.selection.element.style.display = 'none';
+		if (selection.current.element) {
+			if (selection.current.start.join(',') === INVALID_POSITION.join(',')) {
+				selection.current.element.style.display = 'none';
 			} else {
-				const xStart = state.selection.start[0];
-				const xEnd = state.selection.end[0];
-				const yStart = state.selection.start[1];
-				const yEnd = state.selection.end[1];
+				const xStart = selection.current.start[0];
+				const xEnd = selection.current.end[0];
+				const yStart = selection.current.start[1];
+				const yEnd = selection.current.end[1];
 
-				state.selection.element.style.display = 'block';
-				state.selection.element.style.left = `${Math.min(xStart, xEnd)}px`;
-				state.selection.element.style.top = `calc(${Math.min(
+				selection.current.element.style.display = 'block';
+				selection.current.element.style.left = `${Math.min(xStart, xEnd)}px`;
+				selection.current.element.style.top = `calc(${Math.min(
 					yStart,
 					yEnd
 				)}px - 2.5rem)`;
-				state.selection.element.style.width = `${Math.abs(xEnd - xStart)}px`;
-				state.selection.element.style.height = `${Math.abs(yEnd - yStart)}px`;
+				selection.current.element.style.width = `${Math.abs(xEnd - xStart)}px`;
+				selection.current.element.style.height = `${Math.abs(yEnd - yStart)}px`;
 			}
 		}
 	}, []);
 
 	const clearSelection = useCallback(() => {
-		state.selection.start = INVALID_POSITION;
-		state.selection.end = INVALID_POSITION;
+		selection.current.start = INVALID_POSITION;
+		selection.current.end = INVALID_POSITION;
 
 		drawSelection();
 	}, []);
 
-	const isInitialized = () =>
-		!!state.container && !!state.selection && !!state.spacer;
-
 	const onResize = useCallback(() => {
 		queueAnimation(() => {
-			if (state.container) {
+			if (container.current) {
 				drawSelection();
 			}
 		});
-	}, [queueAnimation, state.container, state.spacer, modules, drawSelection]);
+	}, [queueAnimation, drawSelection]);
 
 	const onScroll = useCallback(() => {
-		if (state.container && state.spacer) {
-			const mainRect = state.container.getBoundingClientRect();
-			const spacerRect = state.spacer.getBoundingClientRect();
+		if (container.current && spacer.current) {
+			const mainRect = container.current.getBoundingClientRect();
+			const spacerRect = spacer.current.getBoundingClientRect();
 
 			const expandTop = spacerRect.top - mainRect.top < 150;
 			const expandBottom = spacerRect.bottom - mainRect.bottom < 150;
@@ -105,10 +103,10 @@ export const ModuleCanvasBackdrop: React.FC<{
 			// console.log({ expandTop, expandBottom, expandLeft, expandRight });
 
 			if (expandRight) {
-				state.spacer.style.width = `calc(${spacerRect.width}px + 150px)`;
+				spacer.current.style.width = `calc(${spacerRect.width}px + 150px)`;
 			}
 			if (expandBottom) {
-				state.spacer.style.height = `calc(${spacerRect.height}px + 150px)`;
+				spacer.current.style.height = `calc(${spacerRect.height}px + 150px)`;
 			}
 			if (expandLeft) {
 				// console.log('hmm');
@@ -122,46 +120,46 @@ export const ModuleCanvasBackdrop: React.FC<{
 	const { current: onMouseout } = useRef((/*e: MouseEvent*/) => {});
 
 	useEffect(() => {
-		if (state.container && state.selection && state.spacer) {
+		if (container.current) {
 			onResize();
 
 			window.addEventListener('resize', onResize, false);
 			document.addEventListener('resize', onResize, false);
 			window.addEventListener('mouseout', onMouseout, false);
-			state.container.addEventListener('scroll', onScroll, false);
+			container.current.addEventListener('scroll', onScroll, false);
 
 			return () => {
 				window.removeEventListener('resize', onResize, false);
 				document.removeEventListener('resize', onResize, false);
 				window.addEventListener('mouseout', onMouseout, false);
-				state.container?.removeEventListener('scroll', onScroll, false);
+				container.current?.removeEventListener('scroll', onScroll, false);
 			};
 		}
-	}, [initialized, onResize, state.container, state.selection, state.spacer]);
+	}, [initialized, onResize]);
 
 	const onDrag = useCallback((e: MouseEvent) => {
-		state.selection.end = positionFromMouseEvent(
+		selection.current.end = positionFromMouseEvent(
 			e,
-			state.container as HTMLElement
+			container.current as HTMLElement
 		);
 
 		queueAnimation(drawSelection);
 
-		dispatch(actions.selectionDragContinueAction(state.selection.end));
+		dispatch(actions.selectionDragContinueAction(selection.current.end));
 	}, []);
 
 	const onMouseUp = useCallback((e: MouseEvent) => {
 		dispatch(
 			actions.selectionDragEndAction(
-				positionFromMouseEvent(e, state.container as HTMLElement)
+				positionFromMouseEvent(e, container.current as HTMLElement)
 			)
 		);
 
 		if (
-			Math.abs(state.selection.start[0] - state.selection.end[0]) < 5 &&
-			Math.abs(state.selection.start[1] - state.selection.end[1]) < 5
+			Math.abs(selection.current.start[0] - selection.current.end[0]) < 5 &&
+			Math.abs(selection.current.start[1] - selection.current.end[1]) < 5
 		) {
-			setDeviceButtonPosition(state.selection.start);
+			setDeviceButtonPosition(selection.current.start);
 		}
 
 		queueAnimation(clearSelection);
@@ -173,10 +171,10 @@ export const ModuleCanvasBackdrop: React.FC<{
 	const onMouseDown = useCallback((e: ReactMouseEvent<HTMLDivElement>) => {
 		const position = positionFromMouseEvent(
 			e.nativeEvent,
-			state.container as HTMLElement
+			container.current as HTMLElement
 		);
 
-		state.selection.start = position;
+		selection.current.start = position;
 
 		dispatch(actions.selectionDragStartAction(position));
 
@@ -190,7 +188,7 @@ export const ModuleCanvasBackdrop: React.FC<{
 		<main
 			id="main"
 			ref={(main) => {
-				state.container = main ?? undefined;
+				container.current = main ?? undefined;
 				setInitialized(isInitialized());
 			}}
 			onMouseDown={initialized ? onMouseDown : () => {}}
@@ -199,14 +197,14 @@ export const ModuleCanvasBackdrop: React.FC<{
 			<div
 				id="selection"
 				ref={(div) => {
-					state.selection.element = div ?? undefined;
+					selection.current.element = div ?? undefined;
 					setInitialized(isInitialized());
 				}}
 			/>
 			<div
 				id="spacer"
 				ref={(div) => {
-					state.spacer = div ?? undefined;
+					spacer.current = div ?? undefined;
 					setInitialized(isInitialized());
 				}}
 			/>
