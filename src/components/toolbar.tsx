db6b90558147923de1f864baf4aa3161ac1a4ad7
Rain <rain@synth.kitchen>
2024-12-28 12:40:36 +0100

    re-order toolbar and fix jsx errors

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -100,13 +100,7 @@ export const Toolbar: React.FC<{}> = () => {
 	return (
 		<nav>
 			<section>
-				<input
-					aria-label="patch name"
-					type="text"
-					value={state.name}
-					onChange={handleNameChange}
-					onFocus={handleFocus}
-				/>
+				<Record />
 				<button type="button" onClick={onDownload}>
 					<span className="visually-hidden">download to disk</span>
 					<svg
@@ -120,9 +114,9 @@ export const Toolbar: React.FC<{}> = () => {
 						<path
 							d="M17 17H17.01M17.4 14H18C18.9319 14 19.3978 14 19.7654 14.1522C20.2554 14.3552 20.6448 14.7446 20.8478 15.2346C21 15.6022 21 16.0681 21 17C21 17.9319 21 18.3978 20.8478 18.7654C20.6448 19.2554 20.2554 19.6448 19.7654 19.8478C19.3978 20 18.9319 20 18 20H6C5.06812 20 4.60218 20 4.23463 19.8478C3.74458 19.6448 3.35523 19.2554 3.15224 18.7654C3 18.3978 3 17.9319 3 17C3 16.0681 3 15.6022 3.15224 15.2346C3.35523 14.7446 3.74458 14.3552 4.23463 14.1522C4.60218 14 5.06812 14 6 14H6.6M12 15V4M12 15L9 12M12 15L15 12"
 							stroke="#000000"
-							stroke-width="2"
-							stroke-linecap="round"
-							stroke-linejoin="round"
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
 						/>
 					</svg>
 				</button>
@@ -139,15 +133,22 @@ export const Toolbar: React.FC<{}> = () => {
 						<path
 							d="M17 17H17.01M15.6 14H18C18.9319 14 19.3978 14 19.7654 14.1522C20.2554 14.3552 20.6448 14.7446 20.8478 15.2346C21 15.6022 21 16.0681 21 17C21 17.9319 21 18.3978 20.8478 18.7654C20.6448 19.2554 20.2554 19.6448 19.7654 19.8478C19.3978 20 18.9319 20 18 20H6C5.06812 20 4.60218 20 4.23463 19.8478C3.74458 19.6448 3.35523 19.2554 3.15224 18.7654C3 18.3978 3 17.9319 3 17C3 16.0681 3 15.6022 3.15224 15.2346C3.35523 14.7446 3.74458 14.3552 4.23463 14.1522C4.60218 14 5.06812 14 6 14H8.4M12 15V4M12 4L15 7M12 4L9 7"
 							stroke="#ffffff"
-							stroke-width="2"
-							stroke-linecap="round"
-							stroke-linejoin="round"
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
 						/>
 					</svg>
 					<input type="file" onChange={onUpload} accept="text/json" />
 				</label>
 			</section>
 			<section>
+				<input
+					aria-label="patch name"
+					type="text"
+					value={state.name}
+					onChange={handleNameChange}
+					onFocus={handleFocus}
+				/>
 				<button type="button" onClick={onSave}>
 					<span className="visually-hidden">save to cloud</span>
 					<svg
@@ -161,14 +162,12 @@ export const Toolbar: React.FC<{}> = () => {
 						<path
 							d="M12 21V11M12 11L9 14M12 11L15 14M7 16.8184C4.69636 16.2074 3 14.1246 3 11.6493C3 9.20008 4.8 6.9375 7.5 6.5C8.34694 4.48637 10.3514 3 12.6893 3C15.684 3 18.1317 5.32251 18.3 8.25C19.8893 8.94488 21 10.6503 21 12.4969C21 14.8148 19.25 16.7236 17 16.9725"
 							stroke="#000000"
-							stroke-width="2"
-							stroke-linecap="round"
-							stroke-linejoin="round"
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
 						/>
 					</svg>
 				</button>
-
-				<Record />
 			</section>
 		</nav>
 	);
