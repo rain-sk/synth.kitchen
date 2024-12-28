3a95651b1598e0dda656df80b677bbe034c91a88
Rain <rain@synth.kitchen>
2024-12-28 12:55:31 +0100

    add a bundle analyzer to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { analyzer } from 'vite-bundle-analyzer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), analyzer()],
+});
