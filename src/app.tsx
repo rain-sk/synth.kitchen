8f3a763a4605abb99ec48d594ee285182e1087c8
Rain <rain@synth.kitchen>
2024-12-30 11:26:34 +0100

    re-organize the patch editor into a component

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,13 @@
-import React, { useMemo, useReducer } from 'react';
-import { ModuleCanvasBackdrop } from './components/module-canvas-backdrop';
-import { KeyHandler } from './components/key-handler';
-import { ModuleCanvas } from './components/module-canvas';
+import React, { useReducer } from 'react';
 import { reducer } from './state';
-import { IState, Position, initialState } from './state/types/state';
+import { IState, initialState } from './state/types/state';
 import { Toolbar } from './components/toolbar';
 import { StateContext } from './contexts/state';
 import { DispatchContext } from './contexts/dispatch';
 import { ConnectionContextProvider } from './contexts/connection';
 import { IAction } from './state/actions';
-import { Connections } from './components/connections';
 import { MidiContextProvider } from './contexts/midi';
-import { IModule } from './state/types/module';
+import { PatchEditor } from './components/patch-editor';
 
 const ContextWrapper: React.FC<
 	React.PropsWithChildren<{ state: IState; dispatch: React.Dispatch<IAction> }>
@@ -29,30 +25,10 @@ const ContextWrapper: React.FC<
 
 export const SynthKitchen: React.FC = () => {
 	const [state, dispatch] = useReducer(reducer, initialState);
-
-	const sortedModules = useMemo(
-		() =>
-			Object.entries(state.modulePositions)
-				.sort(
-					([, [ax, ay]], [, [bx, by]]) =>
-						Math.sqrt(Math.pow(ax, 2) + Math.pow(ay, 2)) -
-						Math.sqrt(Math.pow(bx, 2) + Math.pow(by, 2)),
-				)
-				.map(([moduleKey, position]): [IModule, Position] => [
-					state.modules[moduleKey],
-					position,
-				]),
-		[state.modulePositions],
-	);
-
 	return (
 		<ContextWrapper state={state} dispatch={dispatch}>
 			<Toolbar />
-			<ModuleCanvasBackdrop drawOnTop={false}>
-				<KeyHandler />
-				<ModuleCanvas modulesWithPosition={sortedModules} />
-				<Connections />
-			</ModuleCanvasBackdrop>
+			<PatchEditor />
 		</ContextWrapper>
 	);
 };
---
+++ src/components/patch-editor.tsx
@@ -0,0 +1,35 @@
+import React, { useMemo } from 'react';
+import { ModuleCanvasBackdrop } from './module-canvas-backdrop';
+import { KeyHandler } from './key-handler';
+import { ModuleCanvas } from './module-canvas';
+import { Connections } from './connections';
+import { useStateContext } from '../hooks/use-state-context';
+import { IModule } from '../state/types/module';
+import { Position } from '../state/types/state';
+
+export const PatchEditor: React.FC = () => {
+	const { modulePositions, modules } = useStateContext();
+
+	const sortedModules = useMemo(
+		() =>
+			Object.entries(modulePositions)
+				.sort(
+					([, [ax, ay]], [, [bx, by]]) =>
+						Math.sqrt(Math.pow(ax, 2) + Math.pow(ay, 2)) -
+						Math.sqrt(Math.pow(bx, 2) + Math.pow(by, 2)),
+				)
+				.map(([moduleKey, position]): [IModule, Position] => [
+					modules[moduleKey],
+					position,
+				]),
+		[modulePositions, modules],
+	);
+
+	return (
+		<ModuleCanvasBackdrop drawOnTop={false}>
+			<KeyHandler />
+			<ModuleCanvas modulesWithPosition={sortedModules} />
+			<Connections />
+		</ModuleCanvasBackdrop>
+	);
+};
