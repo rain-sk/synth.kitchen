9eb9e451cd162e1d68ebc7048438cc27cc1073b4
Rain <rain@synth.kitchen>
2024-12-31 01:59:50 +0100

    comment out analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { analyzer } from 'vite-bundle-analyzer';
+// import { analyzer } from 'vite-bundle-analyzer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), analyzer()],
+	plugins: [
+		react(),
+		//  analyzer(),
+	],
 });
