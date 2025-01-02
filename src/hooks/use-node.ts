ce841c8331e36a9cedf7a9da4860cd916a95b9a7
Rain <rain@synth.kitchen>
2025-01-02 12:21:40 +0100

    setup a new hook to replace useModuleState

---
+++ src/hooks/use-node.ts
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from 'react';
+import {
+	IModule,
+	IModuleState,
+	ModuleType as Type,
+} from '../state/types/module';
+import { useDispatchContext } from './use-dispatch-context';
+import { actions } from '../state/actions';
+
+export const useNode = <NodeType, ModuleType extends Type>(
+	module: IModule,
+	moduleInit: (
+		node: NodeType,
+		state?: IModuleState[ModuleType],
+	) => IModuleState[ModuleType],
+	nodeFactory: () => NodeType,
+) => {
+	const nodeRef = useRef<NodeType>();
+	if (!nodeRef.current) {
+		nodeRef.current = nodeFactory();
+	}
+
+	const [state, setState] = useState(() =>
+		moduleInit(
+			nodeRef.current as NodeType,
+			module.state as IModuleState[ModuleType],
+		),
+	);
+
+	const dispatch = useDispatchContext();
+
+	useEffect(() => {
+		if (state) {
+			dispatch(actions.updateModuleAction(module.moduleKey, { state }));
+		}
+	}, [dispatch, module.moduleKey, state]);
+
+	return {
+		node: nodeRef.current,
+		state,
+		setState,
+	};
+};
