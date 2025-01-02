4fabe6073015492cf7e99f568a592f435d6f7cd5
Rain <rain@synth.kitchen>
2025-01-02 21:41:31 +0100

    move all patch state into patch-editor

--- src/app.tsx
+++ src/app.tsx
@@ -1,36 +1,6 @@
-import React, { useReducer } from 'react';
-import { reducer } from './patch/state';
-import { blankPatch, IPatchState } from './patch/state/types/patch';
-import { ConnectionContextProvider } from './patch/contexts/connection';
-import { IPatchAction } from './patch/state/actions';
-import { MidiContextProvider } from './patch/contexts/midi';
-import { Toolbar } from './patch/components/toolbar';
+import React from 'react';
 import { PatchEditor } from './patch/components/editor/patch-editor';
-import { PatchLoader } from './patch/components/editor/patch-loader';
-import { PatchContext } from './patch/contexts/patch';
-
-const ContextWrapper: React.FC<
-	React.PropsWithChildren<{
-		state: IPatchState;
-		dispatch: React.Dispatch<IPatchAction>;
-	}>
-> = ({ children, state, dispatch }) => {
-	return (
-		<PatchContext.Provider value={{ ...state, dispatch }}>
-			<ConnectionContextProvider>
-				<MidiContextProvider>{children}</MidiContextProvider>
-			</ConnectionContextProvider>
-		</PatchContext.Provider>
-	);
-};
 
 export const SynthKitchen: React.FC = () => {
-	const [state, dispatch] = useReducer(reducer, blankPatch());
-	return (
-		<ContextWrapper state={state} dispatch={dispatch}>
-			<Toolbar />
-			<PatchEditor />
-			<PatchLoader />
-		</ContextWrapper>
-	);
+	return <PatchEditor />;
 };
--- src/patch/components/editor/patch-editor.tsx
+++ src/patch/components/editor/patch-editor.tsx
@@ -1,14 +1,20 @@
-import React, { useContext, useMemo } from 'react';
+import React, { useMemo, useReducer } from 'react';
 import { ModuleCanvasBackdrop } from './module-canvas-backdrop';
 import { KeyHandler } from './key-handler';
 import { ModuleCanvas } from './module-canvas';
 import { Connections } from './connections';
 import { IModule } from '../../state/types/module';
-import { Position } from '../../state/types/patch';
+import { blankPatch, Position } from '../../state/types/patch';
+import { reducer } from '../../state';
 import { PatchContext } from '../../contexts/patch';
+import { ConnectionContextProvider } from '../../contexts/connection';
+import { MidiContextProvider } from '../../contexts/midi';
+import { Toolbar } from '../toolbar';
+import { PatchLoader } from './patch-loader';
 
 export const PatchEditor: React.FC = () => {
-	const { modulePositions, modules } = useContext(PatchContext);
+	const [state, dispatch] = useReducer(reducer, blankPatch());
+	const { modulePositions, modules } = state;
 
 	const sortedModules = useMemo(
 		() =>
@@ -26,10 +32,18 @@ export const PatchEditor: React.FC = () => {
 	);
 
 	return (
-		<ModuleCanvasBackdrop drawOnTop={false}>
-			<KeyHandler />
-			<ModuleCanvas modulesWithPosition={sortedModules} />
-			<Connections />
-		</ModuleCanvasBackdrop>
+		<PatchContext.Provider value={{ ...state, dispatch }}>
+			<ConnectionContextProvider>
+				<MidiContextProvider>
+					<Toolbar />
+					<ModuleCanvasBackdrop drawOnTop={false}>
+						<KeyHandler />
+						<ModuleCanvas modulesWithPosition={sortedModules} />
+						<Connections />
+					</ModuleCanvasBackdrop>
+					<PatchLoader />
+				</MidiContextProvider>
+			</ConnectionContextProvider>
+		</PatchContext.Provider>
 	);
 };
