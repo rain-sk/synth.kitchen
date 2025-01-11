867c2ba0268ed2b948feb1d101f1431c50f58b71
Rain <rain@synth.kitchen>
2025-01-12 00:09:05 +0100

    update chunking strategy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { analyzer } from 'vite-bundle-analyzer';
+import { analyzer } from 'vite-bundle-analyzer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		//  analyzer(),
-	],
+	plugins: [react(), analyzer()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					webmidi: ['webmidi'],
+					'standardized-audio-context': ['standardized-audio-context'],
+				},
+			},
+		},
+	},
 });
