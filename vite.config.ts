e58adc5d6d02eee4e90019de3f1308b4ab2e8e0c
Rain <rain@synth.kitchen>
2025-01-12 12:17:44 +0100

    comment out analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
-import { analyzer } from 'vite-bundle-analyzer';
+// import { analyzer } from 'vite-bundle-analyzer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), analyzer()],
+	plugins: [
+		react(),
+		// analyzer(),
+	],
 	build: {
 		rollupOptions: {
 			output: {
