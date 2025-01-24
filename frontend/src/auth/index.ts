1d0612f610a237410d4a56ab7590d5e2d66f9662
Rain <rain@synth.kitchen>
2025-01-24 12:25:32 +0100

    fix auth connection on staging/prod

--- frontend/src/auth/index.ts
+++ frontend/src/auth/index.ts
@@ -4,8 +4,8 @@ import Session from 'supertokens-auth-react/recipe/session';
 export const SuperTokensConfig = {
 	appInfo: {
 		appName: 'synth.kitchen',
-		apiDomain: 'https://localhost/api',
-		websiteDomain: 'https://localhost',
+		apiDomain: `https://${process.env.DOMAIN}/api`,
+		websiteDomain: `https://${process.env.DOMAIN}`,
 	},
 	// recipeList contains all the modules that you want to
 	// use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
