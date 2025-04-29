39f9419054405b3081f50a1c622750ea78ab738c
Rain <rain@synth.kitchen>
2025-04-29 11:41:13 +0200

    wip: setup patch content type and api

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,9 +5,12 @@ import { SynthKitchen } from './app';
 
 import './reset.css';
 import './styles.css';
+import { client } from './api/client';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<SynthKitchen />
 	</React.StrictMode>,
 );
+
+console.log(client);
