379240d8c4376609e1172ce89500a6a71f36f1da
Rain <rain@synth.kitchen>
2025-04-29 21:02:01 +0200

    comment-out certbot-specific setup lines

--- scripts/setup-staging.sh
+++ scripts/setup-staging.sh
@@ -59,22 +59,23 @@ echo "server {
 		try_files \$uri \$uri/ /index.html =404;
 	}
 
-  listen 443 ssl; # managed by Certbot
-  listen [::]:443 ssl ipv6only=on; # managed by Certbot
-  ssl_certificate /etc/letsencrypt/live/staging.synth.kitchen/fullchain.pem; # managed by Certbot
-  ssl_certificate_key /etc/letsencrypt/live/staging.synth.kitchen/privkey.pem; # managed by Certbot
-  include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
-  ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
-}
-server {
-  if (\$host = staging.synth.kitchen) {
-    return 301 https://\$host\$request_uri;
-  } # managed by Certbot
+# TODO: uncomment these once certbot stops blocking us
+#   listen 443 ssl; # managed by Certbot
+#   listen [::]:443 ssl ipv6only=on; # managed by Certbot
+#   ssl_certificate /etc/letsencrypt/live/staging.synth.kitchen/fullchain.pem; # managed by Certbot
+#   ssl_certificate_key /etc/letsencrypt/live/staging.synth.kitchen/privkey.pem; # managed by Certbot
+#   include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+#   ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+# }
+# server {
+#   if (\$host = staging.synth.kitchen) {
+#     return 301 https://\$host\$request_uri;
+#   } # managed by Certbot
 
 	listen 80 ;
 	listen [::]:80 ;
-    server_name staging.synth.kitchen;
-    return 404; # managed by Certbot
+    # server_name staging.synth.kitchen;
+    # return 404; # managed by Certbot
 }" > /etc/nginx/sites-available/staging.synth.kitchen
 ln -s /etc/nginx/sites-available/staging.synth.kitchen /etc/nginx/sites-enabled/
 systemctl reload nginx
