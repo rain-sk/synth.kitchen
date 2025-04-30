a6e4ab00c2b627363b190509955d34100190af81
Rain <rain@synth.kitchen>
2025-04-30 13:41:41 +0200

    update server config to include a `url`

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: `http://${(env("HOST"), "localhost")}`,
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
