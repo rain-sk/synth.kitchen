9f8aea3b17fb971796126cf376f995e47dde4688
Rain <rain@synth.kitchen>
2025-06-13 11:56:31 +0200

    idk lol

--- scripts/setup-staging.sh
+++ scripts/setup-staging.sh
@@ -53,19 +53,12 @@ echo "server {
 
   server_name staging.synth.kitchen; # managed by Certbot
 
-  location /cms {
-    rewrite ^/cms/?(.*)$ /$1 break;
-    proxy_pass http://strapi;
-    proxy_http_version 1.1;
-    proxy_set_header X-Forwarded-Host \$host;
-    proxy_set_header X-Forwarded-Server \$host;
-    proxy_set_header X-Real-IP \$remote_addr;
-    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    proxy_set_header X-Forwarded-Proto \$scheme;
-    proxy_set_header Host \$http_host;
-    proxy_set_header Upgrade \$http_upgrade;
-    proxy_set_header Connection "Upgrade";
-    proxy_pass_request_headers on;
+  location /admin {
+    proxy_pass http://localhost:1337;
+  }
+
+  location /api {
+    proxy_pass http://localhost:1337;
   }
 
 	location / {
@@ -74,18 +67,17 @@ echo "server {
 		try_files \$uri \$uri/ /index.html =404;
 	}
 
-# TODO: uncomment these once certbot stops blocking us
-#   listen 443 ssl; # managed by Certbot
-#   listen [::]:443 ssl ipv6only=on; # managed by Certbot
-#   ssl_certificate /etc/letsencrypt/live/staging.synth.kitchen/fullchain.pem; # managed by Certbot
-#   ssl_certificate_key /etc/letsencrypt/live/staging.synth.kitchen/privkey.pem; # managed by Certbot
-#   include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
-#   ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
-# }
-# server {
-#   if (\$host = staging.synth.kitchen) {
-#     return 301 https://\$host\$request_uri;
-#   } # managed by Certbot
+  listen 443 ssl; # managed by Certbot
+  listen [::]:443 ssl ipv6only=on; # managed by Certbot
+  ssl_certificate /etc/letsencrypt/live/staging.synth.kitchen/fullchain.pem; # managed by Certbot
+  ssl_certificate_key /etc/letsencrypt/live/staging.synth.kitchen/privkey.pem; # managed by Certbot
+  include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+  ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+}
+server {
+  if (\$host = staging.synth.kitchen) {
+    return 301 https://\$host\$request_uri;
+  } # managed by Certbot
 
 	listen 80 ;
 	listen [::]:80 ;
@@ -120,9 +112,6 @@ npm run publish:app
 ### BUILD AND DEPLOY THE CMS
 
 # Register the strapi cms service
-echo "upstream strapi {
-    server 127.0.0.1:1337;
-}" > /etc/nginx/conf.d/upstream.conf
 echo "[Unit]
 Description=Strapi CMS
 After=network.target
@@ -144,10 +133,6 @@ WantedBy=multi-user.target
 systemctl daemon-reload
 systemctl start cms
 systemctl enable cms
-
-# Install MySQL
-apt -y install mysql-server
-systemctl enable mysql-server
 
 # Reboot to apply all updates
 reboot
