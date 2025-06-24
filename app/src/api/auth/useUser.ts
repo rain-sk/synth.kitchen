54b7baa422ce81349f719caa88a1d6b3a212a9d1
Rain <rain@synth.kitchen>
2025-06-25 00:01:36 +0200

    use shared type for app useUser hook

--- app/src/api/auth/useUser.ts
+++ app/src/api/auth/useUser.ts
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
+import { AuthenticatedUserInfo } from 'shared';
+
 import { apiBase } from '../uri';
 
 export const useUser = (jwt: string) => {
 	const [loading, setLoading] = useState(true);
-	const [user, setUser] = useState<{ email: string } | undefined>();
+	const [user, setUser] = useState<AuthenticatedUserInfo | undefined>();
 
 	useEffect(() => {
 		(async () => {
@@ -16,6 +18,7 @@ export const useUser = (jwt: string) => {
 							Accept: 'application/json',
 						},
 					}).then((res) => res.json());
+					console.log(response.user);
 					setUser(response.user);
 				} catch (e) {
 					console.error(e);
