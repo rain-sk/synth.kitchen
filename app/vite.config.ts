0cb094751cd467d32a378dc2206bf619f2e19608
Rain <rain@synth.kitchen>
2025-06-27 13:06:29 +0200

    split production build into smaller chunks

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { analyzer } from 'vite-bundle-analyzer';
+import { analyzer } from 'vite-bundle-analyzer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		// analyzer(),
-	],
+	plugins: [react(), analyzer()],
 	build: {
 		rollupOptions: {
 			output: {
 				manualChunks: {
+					'react-use': ['react-use'],
+					'server-name-gen': ['server-name-gen'],
+					'extendable-media-recorder': ['extendable-media-recorder'],
+					'extendable-media-recorder-wav-encoder': [
+						'extendable-media-recorder-wav-encoder',
+					],
 					webmidi: ['webmidi'],
 					'standardized-audio-context': ['standardized-audio-context'],
 				},
