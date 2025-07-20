031c632736524d3091f471dee21e6c4dee7690c4
Rain <rain@synth.kitchen>
2025-07-20 17:55:08 +0200

    cleanup

--- api/src/server.ts
+++ api/src/server.ts
@@ -8,11 +8,7 @@ import { appOrigin, healthRoute } from "./env";
 import { TokenRouter } from "./routes/token";
 
 export const server = express();
-server.use(
-  cors({
-    origin: appOrigin,
-  })
-);
+server.use(cors({ origin: appOrigin }));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 console.log(appOrigin);
