961e25992971d5f0f06e50a181680a526c45ae7d
Rain <rain@synth.kitchen>
2025-07-21 17:13:34 +0200

    restore 1d expiration
    1m was for testing

--- api/src/utils/jwtSign.ts
+++ api/src/utils/jwtSign.ts
@@ -3,5 +3,5 @@ import jsonwebtoken from "jsonwebtoken";
 import { jwtSecret } from "../env";
 
 export const jwtSign = async (data: object) => {
-  return await jsonwebtoken.sign(data, jwtSecret, { expiresIn: "1m" });
+  return await jsonwebtoken.sign(data, jwtSecret, { expiresIn: "1d" });
 };
