bcdfce4c97631f14716a4228d7bf123c6a79e292
Rain <rain@synth.kitchen>
2025-08-24 23:56:37 +0200

    generate some colors

---
+++ app/web/src/lib/shared/utils/color.js
@@ -0,0 +1,17 @@
+// import { Hsluv } from 'hsluv';
+
+// const colors = [];
+
+// for (let i = 0; i < 5; i++) {
+// 	for (let j = 0; j < 5; j++) {
+// 		for (let k = 0; k < 5; k++) {
+// 			var conv = new Hsluv();
+// 			conv.hsluv_h = i * 65 + i * 15;
+// 			conv.hsluv_s = 40 + j * 10;
+// 			conv.hsluv_l = 10 + k * 16;
+// 			conv.hsluvToHex();
+// 			colors.push(conv.hex);
+// 		}
+// 	}
+// }
+// console.log(JSON.stringify(colors));
