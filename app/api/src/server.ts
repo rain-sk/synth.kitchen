33f7c1c50f51877c5e8fa575dec640fa540ef86e
Rain <rain@synth.kitchen>
2025-08-26 18:39:59 +0200

    reorder imports

--- app/api/src/server.ts
+++ app/api/src/server.ts
@@ -3,20 +3,17 @@ import cors from "cors";
 
 import { AuthRouter } from "./routes/auth";
 import { HealthCheckRouter } from "./routes/health";
+import { PatchRouter } from "./routes/patch";
 import { TokenRouter } from "./routes/token";
 
 import { apiBase, appOrigin, healthRoute } from "./env";
-import { PatchRouter } from "./routes/patch";
 
 const ServerRouter = express.Router();
 ServerRouter.use(`/${healthRoute}`, HealthCheckRouter);
 ServerRouter.use("/auth", AuthRouter);
 ServerRouter.use("/patch", PatchRouter);
 ServerRouter.use("/token", TokenRouter);
-// ServerRouter.use("/crash", () => {
-//   process.exit(1);
-// });
-ServerRouter.get("/", (req, res) => {
+ServerRouter.get("/", (_, res) => {
   res.status(400).send("400: Invalid request.");
 });
 
