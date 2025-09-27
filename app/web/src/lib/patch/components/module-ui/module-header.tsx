c69aea4380b8c7c77eea1b53bb37e90f844f8e62
Rain <rain@synth.kitchen>
2025-09-27 21:35:00 +0200

    hide unfinished ModuleMenu

--- app/web/src/lib/patch/components/module-ui/module-header.tsx
+++ app/web/src/lib/patch/components/module-ui/module-header.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Module, randomName } from 'synth.kitchen-shared';
-import { ModuleMenu } from './module-menu';
+// import { ModuleMenu } from './module-menu';
 
 export const ModuleHeader: React.FC<{ module: Module }> = ({ module }) => {
 	const [_] = useState(() => randomName(module.type.toLocaleLowerCase()));
@@ -9,7 +9,7 @@ export const ModuleHeader: React.FC<{ module: Module }> = ({ module }) => {
 			<h2 data-omit>
 				{module.name ? module.name : module.type.toLocaleLowerCase()}
 			</h2>
-			<ModuleMenu />
+			{/* <ModuleMenu /> */}
 		</>
 	);
 };
