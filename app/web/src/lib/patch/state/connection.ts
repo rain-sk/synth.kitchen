dfca066e6ba646915e041a331baf77a62829e11f
Rain <rain@synth.kitchen>
2025-09-27 13:23:54 +0200

    improve log warning about InvalidAccessError

--- app/web/src/lib/patch/state/connection.ts
+++ app/web/src/lib/patch/state/connection.ts
@@ -151,7 +151,9 @@ export const disconnect = (
 			'name' in e &&
 			e.name === 'InvalidAccessError'
 		) {
-			console.log('Captured expected InvalidAccessError in React dev-mode.');
+			console.log(`Captured expected InvalidAccessError in React dev-mode.
+
+Printed below as a warning:`);
 			console.warn(e);
 		} else {
 			console.error(e);
