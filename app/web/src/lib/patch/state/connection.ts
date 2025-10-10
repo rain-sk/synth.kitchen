7dd0e9ce2db8b8c73f3fa8ca85fa62691c0a01cb
Rain <rain@synth.kitchen>
2025-10-10 03:39:20 +0200

    cleanup

--- app/web/src/lib/patch/state/connection.ts
+++ app/web/src/lib/patch/state/connection.ts
@@ -28,7 +28,6 @@ export const connectorKey = (connector: Connector) =>
 export const connectorButton = (key: string) => {
 	const button = document.getElementById(key) as HTMLButtonElement;
 	if (!button) {
-		debugger;
 		throw Error(`Button for connector with key '${key}' not found`);
 	}
 	return button;
