f8beb0337a5a020c4288f838680ef4ea8b58d9dc
Rain <rain@synth.kitchen>
2025-10-17 20:02:18 +0200

    working undo/redo tests

--- cypress/e2e/patch/undo-redo.cy.ts
+++ cypress/e2e/patch/undo-redo.cy.ts
@@ -1,44 +1,78 @@
-describe("undo / redo", () => {
+describe("patch: undo / redo", () => {
   beforeEach(() => {
     cy.visit("/");
+    cy.get("#blank").trigger("click");
+    cy.get("#start").should("exist").click({ force: true });
+  });
+
+  it("creates an undo step when moving a module", () => {
+    cy.get("button#undo").should("be.disabled");
+    cy.get("button#redo").should("be.disabled");
+
+    cy.get("#0").focus().type("{rightarrow}");
+
+    cy.get("button#undo").should("not.be.disabled");
+    cy.get("button#redo").should("be.disabled");
+  });
+
+  it("restores the position of a module with undo/redo", () => {
+    let initialPosition: any = undefined;
+    let modifiedPosition: any = undefined;
+
+    cy.get("#0")
+      .then((div) => {
+        initialPosition = div.position();
+      })
+      .focus()
+      .type("{rightarrow}")
+      .wait(100)
+      .then((div) => {
+        modifiedPosition = div.position();
+        expect(modifiedPosition.left).to.not.equal(initialPosition.left);
+      });
+
+    cy.get("button#undo").trigger("click").wait(100);
+    cy.get("button#undo").should("be.disabled");
+    cy.get("button#redo").should("not.be.disabled");
+    cy.get("#0").then((div) => {
+      expect(div.position().left).to.equal(initialPosition.left);
+    });
+
+    cy.get("button#redo").trigger("click").wait(100);
+    cy.get("button#undo").should("not.be.disabled");
+    cy.get("button#redo").should("be.disabled");
+    cy.get("#0").then((div) => {
+      expect(div.position().left).to.deep.equal(modifiedPosition.left);
+    });
   });
 
   it("creates an undo step when updating a numparam input", () => {
-    cy.visit("/");
+    cy.get("button#undo").should("be.disabled");
+    cy.get("button#redo").should("be.disabled");
 
-    cy.get("blank").click();
+    cy.get("#0 .numparam input")
+      .trigger("click")
+      .type("0.5{enter}")
+      .should("have.value", 0.5);
 
-    cy.get("#start").should("exist");
-    cy.get("#start").click({ force: true });
-
-    cy.get("#undo").should("be.disabled");
-    cy.get("#redo").should("be.disabled");
-
-    // cy.get("#0 .numparam input").should("exist");
-    // cy.get("#0 .numparam input").type("0.5{enter}");
-    // // cy.get("#0 .numparam input").eq(0.5);
-
-    // cy.get("#undo").should("not.be.disabled");
-    // cy.get("#redo").should("be.disabled");
+    cy.get("button#undo").should("not.be.disabled");
+    cy.get("button#redo").should("be.disabled");
   });
 
-  // it("restores the value of a numparam input with undo", () => {
-  //   cy.visit("/");
-  //   cy.get("blank").click();
+  it("restores the value of a numparam input with undo/redo", () => {
+    cy.get("#0 .numparam input")
+      .trigger("click")
+      .type("0.5{enter}")
+      .should("have.value", 0.5);
 
-  //   cy.get("button#start").should("exist");
-  //   // cy.get("button#start").click();
-  //   // cy.get("#undo").should("be.disabled");
-  //   // cy.get("#redo").should("be.disabled");
+    cy.get("button#undo").trigger("click");
+    cy.get("#0 .numparam input").should("have.value", 0.45);
+    cy.get("button#undo").should("be.disabled");
+    cy.get("button#redo").should("not.be.disabled");
 
-  //   // cy.get("#0 .numparam input").type("0.5{enter}");
-  //   // cy.get("#0 .numparam input").eq(0.5);
-
-  //   // cy.get("#undo").should("not.be.disabled");
-  //   // cy.get("#redo").should("be.disabled");
-
-  //   // cy.get("#undo").click();
-  //   // cy.get("#undo").should("be.disabled");
-  //   // cy.get("#redo").should("not.be.disabled");
-  // });
+    cy.get("button#redo").trigger("click");
+    cy.get("#0 .numparam input").should("have.value", 0.5);
+    cy.get("button#undo").should("not.be.disabled");
+    cy.get("button#redo").should("be.disabled");
+  });
 });
