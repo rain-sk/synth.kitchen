34e89f372ab76e5e7c3b65dfce830b9df59670f2
Rain <rain@synth.kitchen>
2025-11-29 13:36:49 +0800

    ensure no pending state upgrades as part of the health check

--- app/api/src/routes/health.router.ts
+++ app/api/src/routes/health.router.ts
@@ -1,12 +1,21 @@
 import express from "express";
+import { patchStateNeedsUpgrade } from "synth.kitchen-shared";
+
 import { AppDataSource } from "../data-source";
+import { SavedPatchState } from "../entity/SavedPatchState";
+
+async function pendingStateUpgrades() {
+  return (await AppDataSource.getRepository(SavedPatchState).find()).some(
+    (entry) => patchStateNeedsUpgrade(entry.state)
+  );
+}
 
 export const HealthCheckRouter = express.Router();
 
 HealthCheckRouter.get("/", async (req, res) => {
-  if (AppDataSource.isInitialized) {
-    res.status(200).send("OK");
-  } else {
+  if (!AppDataSource.isInitialized || (await pendingStateUpgrades())) {
     res.status(500).send("unhealthy");
+  } else {
+    res.status(200).send("OK");
   }
 });
